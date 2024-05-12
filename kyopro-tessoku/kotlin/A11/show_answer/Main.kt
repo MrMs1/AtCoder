67eb1e1fa86253f84c7e885d836045a08ac962e7
Masatomo Murata <masatomo.murata@justincase-tech.com>
2024-05-12 14:15:27 +0900

    A11 end

---
+++ kyopro-tessoku/kotlin/A11/show_answer/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val (n, x) = readLine()!!.split(" ").map { it.toInt() }
+    val nums = readLine()!!.split(" ").map { it.toInt() }
+    println(binarySearch(1, n, nums, x))
+}
+
+fun binarySearch(
+    start: Int,
+    max: Int,
+    list: List<Int>,
+    target: Int,
+): Int? {
+    val harf = (max + start) / 2
+    if (list[harf] > target) {
+        return binarySearch(start, harf - 1, list, target)
+    }
+    if (list[harf] == target) {
+        return harf + 1
+    }
+    if (list[harf] < target) {
+        return binarySearch(harf + 1, max, list, target)
+    }
+    return null
+}
